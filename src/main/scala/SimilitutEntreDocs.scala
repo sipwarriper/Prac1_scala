e7d8645d010c416962356d8efae37e5b00f3fb84
sipwarriper <sipwarriper@gmail.com>
2018-11-19 14:39:01 +0100

    vector model space fet

--- src/main/scala/SimilitutEntreDocs.scala
+++ src/main/scala/SimilitutEntreDocs.scala
@@ -1,41 +1,69 @@
 import scala.io.Source
+import scala.math.sqrt
 
 object SimilitutEntreDocs extends App {
 
-  def freq(text:String, n:Int):List[(String, Int)] = normalitza(text).split(" +").sliding(n).toList.map(_.mkString(" ")).groupBy(identity).mapValues(_.length).toList
+  def freq(text:String, n:Int):List[(String, Int)] =
+    normalitza(text).split(" +").sliding(n).toList.map(_.mkString(" ")).groupBy(identity).mapValues(_.length).toList
 
-  def normalitza(text:String):String = text.map(c=> if(c.isLetter) c else ' ').toLowerCase().trim
+  def normalitza(text:String):String =
+    text.map(c=> if(c.isLetter) c else ' ').toLowerCase().trim
 
   def nonStopFreq(text:String, stop:List[String], n:Int):List[(String, Int)] =
     freq(text,n).filter{a => !stop.contains(a._1)}
 
-  	override def main(args: Array[String]): Unit = {
-      val filename = "pg11.txt"
-      val fileContents = Source.fromFile(filename).mkString
+  def cosinesim (text1:String, text2:String, stop:List[String]):Double = {
+    val mesfrequent1 = mesFrequent(text1,stop, 1)._2
+    val mesfrequent2 = mesFrequent(text2,stop, 1)._2
+    var txt1 = nonStopFreq(text1, stop, 1).map{a=> (a._1, a._2.toDouble/mesfrequent1)}
+    var txt2 = nonStopFreq(text2, stop, 1).map{a=> (a._1, a._2.toDouble/mesfrequent2)}
+    //part lenta
+    txt1 = (txt1 ::: (for (b<-txt2 if !txt1.toMap.contains(b._1)) yield (b _1, 0.0))) sortBy(_._1) //busquem les paraules q no tenim a txt1 de txt2 i les afegim amb frequencia = 0 a txt1 Al final ordenem alfabeticament, per tenir el mateix ordre en els dos vectors!
+    txt2 = (txt2 ::: (for (b<-txt1 if !txt2.toMap.contains(b._1)) yield (b _1, 0.0))) sortBy(_._1)
+    //fi part lenta
+    (for ((a, b) <- txt1 zip txt2) yield a._2 * b._2).foldLeft(0.0)(_ + _) /( sqrt(txt1.foldLeft(0.0){(a,b)=> a+(b._2*b._2)}) * sqrt(txt2.foldLeft(0.0){(a,b)=> a+(b._2*b._2)}) )
+  }
 
-      val llistaFreq = freq(fileContents,1).sortBy(-_._2)
-      val nParules = llistaFreq.foldLeft(0){(a,b) => b._2+a}
-      val diff = llistaFreq.size
-      println(String.format("%-20s %-10s %-20s %-20s", "Num de Parules:", nParules.toString, "Diferents", diff.toString))
-      println(String.format("%-20s %-20s %-20s ", "Paraules", "ocurrencies", "frequencia"))
-      println("------------------------------------------------------")
-      val sFormat = "%-20s %-20s %-1.3f "
-      for (p <- llistaFreq.slice(0,10)) println(String.format(sFormat, p._1, p._2.toString, (p._2*100.0/nParules).toFloat:java.lang.Float))
-
-      println("\n\nNONSTOP EXAMPLE\n")
-
-      val filename2 = "english-stop.txt"
-      val stopWords = Source.fromFile(filename2).getLines.toList
-      val llistaNonStop = nonStopFreq(fileContents, stopWords, 1).sortBy(-_._2)
-      println(String.format("%-20s %-20s %-20s ", "Paraules", "ocurrencies", "frequencia"))
-      println("------------------------------------------------------")
-      for (p <- llistaNonStop.slice(0,10)) println(String.format(sFormat, p._1, p._2.toString, (p._2*100.0/nParules).toFloat:java.lang.Float))
-
-      println("\n\nn-grames\n")
-      val llistaNgrames = freq(fileContents, 3).sortBy(-_._2)
-      for (p <- llistaNgrames slice (0, 10)) println(String.format("%-30s %-5s", p._1, p._2.toString))
+  def mesFrequent(text:String, stop:List[String], n:Int) = nonStopFreq(text, stop, n).maxBy(_._2)
 
 
-    }
+
+  override def main(args: Array[String]): Unit = {
+    val filename = "pg11.txt"
+    val fileContents = Source.fromFile(filename).mkString
+
+    val llistaFreq = freq(fileContents,1).sortBy(-_._2)
+    val nParules = llistaFreq.foldLeft(0){(a,b) => b._2+a}
+    val diff = llistaFreq.size
+    println(String.format("%-20s %-10s %-20s %-20s", "Num de Parules:", nParules.toString, "Diferents", diff.toString))
+    println(String.format("%-20s %-20s %-20s ", "Paraules", "ocurrencies", "frequencia"))
+    println("------------------------------------------------------")
+    val sFormat = "%-20s %-20s %-1.3f "
+    for (p <- llistaFreq.slice(0,10)) println(String.format(sFormat, p._1, p._2.toString, (p._2*100.0/nParules).toFloat:java.lang.Float))
+
+    println("\n\nNONSTOP EXAMPLE\n")
+
+    val filename2 = "english-stop.txt"
+    val stopWords = Source.fromFile(filename2).getLines.toList
+    val llistaNonStop = nonStopFreq(fileContents, stopWords, 1).sortBy(-_._2)
+    println(String.format("%-20s %-20s %-20s ", "Paraules", "ocurrencies", "frequencia"))
+    println("------------------------------------------------------")
+    for (p <- llistaNonStop.slice(0,10)) println(String.format(sFormat, p._1, p._2.toString, (p._2*100.0/nParules).toFloat:java.lang.Float))
+
+    println("\n\nn-grames\n")
+    val llistaNgrames = freq(fileContents, 3).sortBy(-_._2)
+    for (p <- llistaNgrames slice (0, 10)) println(String.format("%-30s %-5s", p._1, p._2.toString))
+
+    val filename3 = "pg11-net.txt"
+    val fileContents2 = Source.fromFile(filename3).mkString
+    println("\n\ncosinesim\n")
+    val hi = cosinesim(fileContents,fileContents2,stopWords)
+    println(hi)
+
+
+
+
+
+  }
 }
 
